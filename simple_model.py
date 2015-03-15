973fa091895d4a11ba7db6958f4858270764198b
Joerg Rings <mail@rings.de>
2015-03-15 15:46:22 -0500

    Simple cross val test

--- simple_model.py
+++ simple_model.py
@@ -3,26 +3,36 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.cross_validation import cross_val_score
 
-labels = pd.read_csv("../train_labels.csv")
-train = pd.read_csv("../train_values.csv")
-test = pd.read_csv("../test_values.csv")
+def prepare_data():
+    labels = pd.read_csv("../train_labels.csv")
+    train = pd.read_csv("../train_values.csv")
+    test = pd.read_csv("../test_values.csv")
 
+    ids = train.pop("id")
+    test_ids = test.pop("id")
 
-model = LogisticRegression()
-ids = train.pop("id")
-test_ids = test.pop("id")
+    combined = pd.concat((train, test))
 
+    #Expand categories and impute missing numerical values
+    combined = pd.get_dummies(combined)
+    for col in combined:
+        if col.startswith("n_"):
+            filler = np.nanmean(combined[col])
+            combined[col].fillna(filler, inplace=True)
 
-combined = pd.concat((train, test))
+    #Split up again
+    train = combined.iloc[:len(ids)]
+    test = combined.iloc[len(ids):]
 
-#Expand categories and impute missing numerical values
-combined = pd.get_dummies(combined)
-for col in combined:
-    if col.startswith("n_"):
-        filler = np.nanmean(combined[col])
-        combined[col].fillna(filler, inplace=True)
+    return train, test, labels, ids, test_ids
 
-#Split up again
-train = combined.iloc[:len(ids)]
-test = combined.iloc[len(ids):]
+def main():
+    train, test, labels, ids, test_ids = prepare_data()
+    model = LogisticRegression()
+    print(np.mean(cross_val_score(np.array(train), np.array(labels["service_a"]),
+                              cv=4, n_jobs=2, scoring="accuracy")))
+    
 
+    
+if __name__ == "__main__":
+    main()
