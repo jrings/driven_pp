2ab28da06232b93d80ea061791226cf2b947a5d5
Joerg Rings <mail@rings.de>
2015-03-19 19:35:43 -0500

    Start of testing Shogun multilabel SVM ability

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # driven_pp
 Drivendata PP Competition
+
+
+## SHOGUN
+
+http://www.shogun-toolbox.org/ has some notebooks documenting examples, and medicore installation instructions
---
+++ use_shogun.py
@@ -0,0 +1,25 @@
+from modshogun import RealFeatures, MultilabelSOLabels, MultilabelModel
+
+import numpy as np
+import pandas as pd
+
+from simple_model import read_data
+
+def main():
+
+    train, test, labels, ids, test_ids = read_data()
+
+    # Build Multilabel object
+    labels = labels.set_index("id")
+    labels_so = MultilabelSOLabels(labels.shape[0], labels.shape[1])
+    for i, (_, row) in enumerate(labels.iterrows()):
+        labels_so.set_sparse_label(int(i), np.array(row, dtype=np.int32))
+
+    # Build train object
+    train_features = RealFeatures(np.c_(np.array(train), np.ones(train.shape[0])))
+    test_features = RealFeatures(np.c_(np.array(train), np.ones(train.shape[0])))
+        
+    
+
+if __name__ == "__main__":
+    main()
